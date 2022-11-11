3706627c8e4964a3c94f9b29c58ee502af98e03c
Dmitrii Oreshkin <71903279+dmitrii-subb@users.noreply.github.com>
2022-11-11 23:23:10 +0300

    Update SidebarPage.jsx

--- react-chat/src/pages/SidebarPage/SidebarPage.jsx
+++ react-chat/src/pages/SidebarPage/SidebarPage.jsx
@@ -10,7 +10,7 @@ function SidebarPage({openChat}) {
         <>
             <SidebarHeader />
             <SidebarBody openChat={openChat} />
-            {/* <FloatButton /> */}
+            <FloatButton />
         </>
     );
 }
