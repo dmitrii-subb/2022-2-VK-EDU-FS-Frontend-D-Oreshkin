215ce234d16f699f6188a902a532335cebda4877
dmitrii <xbox2002@list.ru>
2022-12-30 15:29:02 +0300

    remove conflicts

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -11,8 +11,6 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import { RequireAuth } from "./hoc/RequireAuth";
 
 import { connect, useSelector } from "react-redux";
-
-import { connect } from "react-redux";
 import { setViewAction } from "./actions/viewAction";
 import { renderNewMessageAction } from "./actions/messageAction";
 
